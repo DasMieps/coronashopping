153d3bdd747bfc723a554b76606ab9538e7f73c0
krenn <S1710456018@students.fh-hagenberg.at>
2020-05-18 21:21:13 +0200

    abgabe test1

--- client/coronashopping20/src/app/home/home.component.ts
+++ client/coronashopping20/src/app/home/home.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
     selector : 'bs-home' ,
     template : `
         <div class="ui container">
-            <h1>Home</h1>
-            <p>Das ist KWM-Shopping gegen Corona.</p>
+            <h1>Wilkommen</h1>
+            <p>Das ist KWM-Shopping gegen Corona. <br/>Wir wollen Sie beim einkaufen unterstützten und bringen Hilfesuchende und Einkäufer zusammen!</p>
             <a routerLink="../shoppinglists" class="ui red button">
                 Übersicht ansehen
                 <i class="right arrow icon"></i>
